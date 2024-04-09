57acab9e06f26bfc470b3c8ee0ca0a88bbc6709e
Denkata1890 <128889492+Denkata1890@users.noreply.github.com>
2024-04-09 18:58:29 +0300

    Add files via upload

---
+++ BeverageRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PalateParadise.Data;
+using PalateParadise.Interfaces;
+using PalateParadise.Models;
+using System.Diagnostics;
+
+namespace PalateParadise.Repository
+{
+    public class BeverageRepository:IBeverageRepostiroy
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BeverageRepository(ApplicationDbContext context)
+        {
+           _context = context;
+        }
+
+        public bool Add(Beverage beverage)
+        {
+            _context.Add(beverage);
+            return Save();
+        }
+
+        public bool Delete(Beverage beverage)
+        {
+            _context.Remove(beverage);
+            return Save();
+        }
+
+        public async Task<List<Beverage>> GetAllAsync()
+        {
+            return await _context.Beverages.ToListAsync();
+        }
+
+        public async Task<Beverage> GetById(int id)
+        {
+            return await _context.Beverages.FirstOrDefaultAsync(b => b.BeverageId == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+
+        }
+
+        public bool Update(Beverage beverage)
+        {
+            _context.Update(beverage);
+            return Save();
+        }
+    
+    }
+}
---
+++ MenuRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using PalateParadise.Data;
+using PalateParadise.Interfaces;
+using PalateParadise.Models;
+
+namespace PalateParadise.Repository
+{
+    public class MenuRepository:IMenuRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public MenuRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool Add(Menu menu)
+        {
+            _context.Add(menu);
+            return Save();
+        }
+
+        public bool Delete(Menu menu)
+        {
+            _context.Remove(menu);
+            return Save();
+        }
+
+        public async Task<List<Menu>> GetAllAsync()
+        {
+            return await _context.Menus.ToListAsync();
+        }
+
+
+        public async Task<Menu> GetById(int id)
+        {
+            return await _context.Menus.FirstOrDefaultAsync(c => c.MenuId == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0;
+        }
+
+        public bool Update(Menu menu)
+        {
+            _context.Update(menu);
+            return Save();
+        }
+    }
+}
